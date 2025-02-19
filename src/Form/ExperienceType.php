fd474eb893653d3e24011e24d61bcda367d487de
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-02-19 12:03:39 +0100

    Amélioration du formulaire ExperienceType avec des étiquettes et des classes CSS pour une meilleure présentation, ajout de contraintes de validation pour le champ pays.

--- src/Form/ExperienceType.php
+++ src/Form/ExperienceType.php
@@ -3,31 +3,76 @@
 namespace App\Form;
 
 use App\Entity\Cv;
-use App\Entity\Experience;
-use App\Entity\Skill;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Skill;
+use App\Entity\Experience;
+use Doctrine\DBAL\Types\ArrayType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class ExperienceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_start')
-            ->add('date_end')
-            ->add('title')
-            ->add('organization')
-            ->add('description')
-            ->add('postal_code')
-            ->add('city')
-            ->add('country')
+            ->add('date_start', TextType::class, [
+                'label' => 'Date de début',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+            ])
+            ->add('date_end', TextType::class, [
+                'label' => 'Date de fin',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+
+            ])
+            ->add('organization', TextType::class, [
+                'label' => 'organisation',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+            ])
+            ->add('postal_code', TextType::class, [
+                'label' => 'Code postal',
+                'label_attr' => ['class' => 'block'],
+
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'ville',
+                'label_attr' => ['class' => 'block'],
+
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'pays',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
             ->add('skills', EntityType::class, [
+                'label' => "Compétences",
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
                 'class' => Skill::class,
-'choice_label' => 'id',
-'multiple' => true,
+                'choice_label' => 'id',
+                'multiple' => true,
             ])
         ;
     }
