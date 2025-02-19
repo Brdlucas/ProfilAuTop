1d8a6b37948ce38a6835213933837c7c624fcc21
Mohammad ABDUL <abdul.opto@gmail.com>
2025-02-19 09:05:08 +0100

    Modification des routes des contrôleurs pour les regrouper sous le préfixe '/profil'.

--- src/Controller/OfferController.php
+++ src/Controller/OfferController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/offer')]
+#[Route('/profil/offer')]
 final class OfferController extends AbstractController{
     #[Route(name: 'app_offer_index', methods: ['GET'])]
     public function index(OfferRepository $offerRepository): Response
