0c39295f0f66e79fbcbcbcd43ffca7b5c340a0d2
Nicolas K <nicolaskay95200@gmail.com>
2025-02-20 10:32:42 +0100

    Amélioration du formulaire de formation avec des contraintes de validation et des étiquettes en français

--- src/Form/FormationType.php
+++ src/Form/FormationType.php
@@ -8,29 +8,138 @@ use App\Entity\Skill;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class FormationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_start')
-            ->add('date_end')
-            ->add('title')
-            ->add('organization')
-            ->add('description')
-            ->add('postal_code')
-            ->add('city')
-            ->add('country')
-            ->add('level')
-            ->add('is_graduated')
-            ->add('degree')
+            ->add('date_start', TextType::class, [
+                'label' => 'Date de début',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('date_end', TextType::class, [
+                'label' => 'Date de fin',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('title', TextType::class, [
+                'label' => 'Titre',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+
+            ])
+            ->add('organization', TextType::class, [
+                'label' => 'organisation',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'description',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('postal_code', TextType::class, [
+                'label' => 'Code postal',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    ),
+                    new Length(
+                        min: 5,
+                        max: 5,
+                        minMessage: "Le code postal doit faire au moins {{ limit }} caractères",
+                        maxMessage: "Le code postal ne peut pas dépasser {{ limit }} caractères"
+                    ),
+                    new Regex(
+                        pattern: "/^[0-9]{5}$/",
+                        message: "Le code postal doit être composé de chiffres uniquement"
+                    )
+                ]
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'ville',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('country', CountryType::class, [
+                'label' => 'pays',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1']
+            ])
+            ->add('level', TextType::class, [
+                'label' => 'Niveau',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('is_graduated', TextType::class, [
+                'label' => 'Diplômé',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1'],
+                'constraints' => [
+                    new NotBlank(
+                        message: "Ce champs est obligatoire"
+                    )
+                ]
+            ])
+            ->add('degree', TextType::class, [
+                'label' => 'Diplôme',
+                'label_attr' => ['class' => 'block'],
+                'attr' => ['class' => 'border border-gray-300 rounded-md w-full p-1']
+            ])
             ->add('skills', EntityType::class, [
+                'label' => "Compétences",
+                'label_attr' => ['class' => 'w-full bg-red-800'],
+                'attr' => ['class' => 'custom-wrapper'],
                 'class' => Skill::class,
-'choice_label' => 'id',
-'multiple' => true,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
             ])
         ;
     }
